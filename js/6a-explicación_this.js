03be29d0cf76e9f47b911f10fe4b8bfffacbaa81
abelcg <abelcg86@gmail.com>
2023-08-27 18:41:37 -0300

    add comments to explicacion this

--- js/6a-explicación_this.js
+++ js/6a-explicación_this.js
@@ -1,5 +1,31 @@
 /* 
 keyword this: hace referencia al objeto actual. Sirve para obtener todo lo que tiene dicho objeto
+
+si defino el método deposito: ()=>{
+    console.log(this)
+}
+
+veremos que this hace referencia al objeto global windows por lo cuál no se recomiendo usar arrow fuctions
+para definir métodos
+
+si lo definimos como deposito(){
+    this  hará referencia al objeto cuenta, es decir una autoreferenciación
+}
+
+ deposito(){
+        this.saldo = this.saldo + 100
+    },
+    retiro(){
+        return this.saldo = this.saldo - 50
+    }
+
+
+    invoco
+cuenta.deposito()
+console.log(cuenta.saldo)
+cuenta.retiro()
+console.log(cuenta.saldo)
+como los métodos son funciones admiten parámetros. Incluimos el parámetro cantidad
 */
 
 const cuenta = {
